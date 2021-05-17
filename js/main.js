ce7fd8057154c8f69367ad1d4329c863a7c6814d
Jakub Parlej <jakubparlej@gmail.com>
2021-05-17 18:59:53 +0200

    add cards section with icons and wipers button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+const openFrontTrunk = document.querySelector('#openFrontTrunk');
+const openRearTrunk = document.querySelector('#openRearTrunk');
+const openChargingPlug = document.querySelector('#openChargingPlug');
+
+openFrontTrunk.addEventListener('click',
+  () => {
+    if (openFrontTrunk.textContent === "OPEN") {
+      openFrontTrunk.textContent = "CLOSE";
+    } else {
+      openFrontTrunk.textContent = "OPEN";
+    }
+  });
+
+openRearTrunk.addEventListener('click',
+  () => {
+    if (openRearTrunk.textContent === "OPEN") {
+      openRearTrunk.textContent = "CLOSE";
+    } else {
+      openRearTrunk.textContent = "OPEN";
+    }
+  });
+
+openChargingPlug.addEventListener('click',
+  () => {
+    let flag = true;
+
+    if (flag === true) {
+      openChargingPlug.textContent = "CLOSE";
+    } else if (openChargingPlug.textContent === "CLOSE") {
+      openChargingPlug.textContent = "OPEN";
+    }
+  });
